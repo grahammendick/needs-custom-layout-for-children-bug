d76cdc1c23db5ebe2ac98506c980ecc927e53755
Graham Mendick <graham.mendick@gmail.com>
2022-06-25 14:56:17 +0100

    Setup for navigation on new arch on android

--- newarch/android/app/src/main/jni/MainComponentsRegistry.cpp
+++ newarch/android/app/src/main/jni/MainComponentsRegistry.cpp
@@ -3,6 +3,10 @@
 #include <CoreComponentsRegistry.h>
 #include <fbjni/fbjni.h>
 #include <react/renderer/componentregistry/ComponentDescriptorProviderRegistry.h>
+#include <react/renderer/components/navigation-react-native/ComponentDescriptors.h>
+#include <react/renderer/components/navigation-react-native/NVActionBarComponentDescriptor.h>
+#include <react/renderer/components/navigation-react-native/NVSearchBarComponentDescriptor.h>
+#include <react/renderer/components/navigation-react-native/NVTitleBarComponentDescriptor.h>
 #include <react/renderer/components/rncore/ComponentDescriptors.h>
 
 namespace facebook {
@@ -19,6 +23,29 @@ MainComponentsRegistry::sharedProviderRegistry() {
   //
   // providerRegistry->add(concreteComponentDescriptorProvider<
   //        AocViewerComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVActionBarComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVBarButtonComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVBottomAppBarComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVBottomSheetComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVCollapsingBarComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVCoordinatorLayoutComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVExtendedFloatingActionButtonComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVFloatingActionButtonComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVNavigationBarComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVNavigationStackComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVSceneComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVSearchBarComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVSharedElementComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVStatusBarComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVTabBarItemComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVTabBarComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVTabBarPagerComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVTabBarPagerRTLComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVTabLayoutComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVTabLayoutRTLComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVTabNavigationComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVTitleBarComponentDescriptor>());
+  providerRegistry->add(concreteComponentDescriptorProvider<NVToolbarComponentDescriptor>());
   return providerRegistry;
 }
 
